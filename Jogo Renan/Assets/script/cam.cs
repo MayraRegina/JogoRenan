4e8c37d7e2f2477afdb565be2edb5ee2f1d13faf
juisa06 <julia.isadora@escolar.ifrn.edu.br>
2023-08-03 15:31:45 -0300

    script camera

--- Jogo Renan/Assets/script/cam.cs
+++ Jogo Renan/Assets/script/cam.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cam : MonoBehaviour
+
 {
+    private Transform player;
+    public float smooth;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        if (player.position.x >=0 && player.position.x <=164.2)
+        {
+            Vector3 following = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime); 
+        }
+
+        if (player.position.y <=7.9 && player.position.y >=0)
+        {
+            Vector3 following = new Vector3(transform.position.x, player.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime); 
+        }
         
     }
 }
